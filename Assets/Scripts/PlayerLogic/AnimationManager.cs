0b429290be7177278a5c5455ed2ed8d0ea75de3c
Vasiliy-Enyutin <vasiliyqqerty@gmail.com>
2022-11-10 19:00:06 +0300

    [Add] Player prefab. Delete trash from AnimationManager. Move cameras to Cameras container

--- Assets/Scripts/PlayerLogic/AnimationManager.cs
+++ Assets/Scripts/PlayerLogic/AnimationManager.cs
@@ -8,26 +8,13 @@ namespace PlayerLogic
         [SerializeField] private CharacterController _characterController;
         [SerializeField] private Animator _animator;
 
-        private readonly int _moveHash = Animator.StringToHash("Walking");
-        private readonly int _idleStateHash = Animator.StringToHash("Idling");
+        private readonly int _moveHash = Animator.StringToHash("Running");
 
         private Action _currentAnimationEndCallback;
-
-        public void ResetToIdle()
-        {
-            _animator.Play(_idleStateHash, -1);
-        }
 
         private void Update()
         {
             _animator.SetFloat(_moveHash, _characterController.velocity.magnitude, 0.1f, Time.deltaTime);
-        }
-
-        // Triggered from animation
-        private void CallbackAnimationEnd()
-        {
-            _currentAnimationEndCallback?.Invoke();
-            _currentAnimationEndCallback = null;
         }
     }
 }
