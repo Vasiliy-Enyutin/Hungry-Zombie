5e925000dad903ab4102961716bd2f1c6282fea3
Vasiliy-Enyutin <vasiliyqqerty@gmail.com>
2022-11-11 18:19:00 +0300

    [Add] [PACKAGE] Joystick Pack

--- Assets/Scripts/PlayerLogic/PlayerInputService.cs
+++ Assets/Scripts/PlayerLogic/PlayerInputService.cs
@@ -4,6 +4,15 @@ namespace PlayerLogic
 {
     public class PlayerInputService : MonoBehaviour, IPlayerInputService
     {
-        public Vector3 MoveDirection => new(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        public Vector3 MoveDirection
+        {
+            get
+            {
+                if (SystemInfo.deviceType == DeviceType.Handheld)
+                    return new();
+                
+                return new(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+            }
+        }
     }
 }
