85ff13ff93e159b500676365c5a7215cfda9e75d
Vasiliy-Enyutin <vasiliyqqerty@gmail.com>
2022-11-11 17:57:48 +0300

    [Add] [META] Enemy, Enemy spawner for spawn enemy to spawn points

---
+++ Assets/Scripts/EnemyLogic/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace EnemyLogic
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject _meleeEnemyPrefab;
+        [SerializeField] private List<Transform> _enemySpawnPoints;
+
+        private void Awake()
+        {
+            SpawnEnemies();
+        }
+
+        private void SpawnEnemies()
+        {
+            _enemySpawnPoints.ForEach(spawnPoint => Instantiate(_meleeEnemyPrefab, spawnPoint.position, Quaternion.identity));
+        }
+    }
+}
